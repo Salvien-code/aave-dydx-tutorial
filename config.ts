0987febad4ec633358974eecae46f7c6eca8d362
Simon Samuel <salviensky@gmail.com>
2022-10-04 21:34:48 +0100

    Still haven't figured out the exact AAVE SDK

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
 import { Network, Alchemy } from "alchemy-sdk";
+import axios from "axios";
 import "dotenv/config";
 
-const { ALCHEMY_API_KEY } = process.env;
+const { ALCHEMY_API_KEY, AAVE_STAGING_URL } = process.env;
 
 // Alchemy Settings
 const settings = {
@@ -9,10 +10,20 @@ const settings = {
   network: Network.ETH_GOERLI,
 };
 
-const alchemy = new Alchemy(settings);
+const alchemyInstance = new Alchemy(settings);
 
+export const axiosInstance = axios.create({
+  baseURL: AAVE_STAGING_URL,
+  responseType: "json",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Exported config
 const config = {
-  alchemy,
+  alchemyInstance,
+  axiosInstance,
 };
 
 export { config };
--- scripts/aave-dydx.ts
+++ scripts/aave-dydx.ts
@@ -1,15 +1,24 @@
 import { config } from "../config";
 import { AAVE_WETH_ADDRESS, LINK_ADDRESS, WALLET_ADDRESS } from "../constants";
+import { LendingPoolInterfaceV2 } from "@aave/protocol-js";
 
-const { alchemy } = config;
+const { alchemyInstance, axiosInstance } = config;
 
 async function main() {
-  const tokens = await alchemy.core.getTokenBalances(WALLET_ADDRESS, [
+  const tokens = await alchemyInstance.core.getTokenBalances(WALLET_ADDRESS, [
     LINK_ADDRESS,
     AAVE_WETH_ADDRESS,
   ]);
 
   console.log(tokens);
+
+  // Borrow Assets from AAVE
+
+  // Open Position on dydx
+
+  // Close Position on dydx
+
+  // Repay Assets to AAVE
 }
 
 main().catch((error) => {
